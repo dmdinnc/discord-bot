35fa390efd086cc7f2325f8c58744aeb2d3714dd
dmdinnc <38630939+dmdinnc@users.noreply.github.com>
2024-03-31 22:45:53 -0400

    add new deploy folder, change deploy process, new graceful exit (#1)

--- src/main/java/com/dmdinnc/discordbot/MainController.java
+++ src/main/java/com/dmdinnc/discordbot/MainController.java
@@ -1,10 +1,13 @@
 package com.dmdinnc.discordbot;
 
 import com.dmdinnc.discordbot.commands.ExampleCommand;
+import com.dmdinnc.discordbot.commands.ExampleCommandWithStorage;
 import com.dmdinnc.discordbot.listeners.ExampleListener;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+
+import java.time.Duration;
 
 public class MainController {
 
@@ -20,5 +23,21 @@ public class MainController {
                 GatewayIntent.GUILD_MESSAGES, GatewayIntent.GUILD_MEMBERS).build();
         api.addEventListener(new ExampleListener(configurationManager));
         api.addEventListener(exampleCommand);
+        api.addEventListener(new ExampleCommandWithStorage());
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Shutting down bot...");
+            try {
+                if (api.awaitShutdown(Duration.ofSeconds(15))) {
+                    System.out.println("Bot has been shut down successfully.");
+                } else {
+                    api.shutdownNow();
+                    System.out.println("Bot has been shut down forcefully.");
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                api.shutdownNow();
+            }
+        }));
     }
 }
